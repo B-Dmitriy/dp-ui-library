606e196601af5171e3e6b6011be96492d4ca4936
username91355 <personalmailD13@gmail.com>
2022-10-23 19:05:48 +0300

    config(eslint): added eslint

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: {
+		path: './src/index.tsx',
+	},
+	output: {
+		path: path.join(__dirname, '..', '/dist'),
+		filename: "[name].[contenthash].js",
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, '..', 'public', 'index.html'),
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	devServer: {
+		port: 8080,
+		
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+	},
+};
