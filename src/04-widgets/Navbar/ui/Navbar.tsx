41b6fc24090143ba30fbf29794b982075d625471
B-Dmitriy <balyaevds.main@gmail.com>
2023-08-20 12:57:58 +0300

    feat(theme): add theme provider

--- src/04-widgets/Navbar/ui/Navbar.tsx
+++ src/04-widgets/Navbar/ui/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import cls from './Navbar.module.scss';
 import {Link} from "react-router-dom";
+import {useTheme} from "../../../07-shared/providers/themeProvider/lib/useTheme";
 export const Navbar = () => {
+    const {  theme, themeList, setTheme} = useTheme()
     return (
         <div className={cls.Navbar}>
             <Link to={"/todos"}>Todos</Link>
             <Link to={"/auth"}>Auth</Link>
+
+            <button ></button>
         </div>
     );
 };
